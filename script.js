521f35eeb91b16cafbad911f2f37fffa4143e358
rizwanmajeed2000 <rizwanmajeed2000@gmail.com>
2026-03-15 11:28:14 +0500

    Practice 6

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("result").innerHTML = 2;
+        document.getElementById("demo").firstChild.nodeValue;
+    document.getElementById("result1").innerHTML = 4;
+        document.getElementById("demo").childNodes[0].nodeValue;
+}
